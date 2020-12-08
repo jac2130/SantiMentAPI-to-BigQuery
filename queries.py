a0a362819bc430c4fbaed0bf4665f79ba0acd2c7
johannes <jac2130@gmail.com>
2020-12-08 18:03:06 +0000

    saving some things

--- queries.py
+++ queries.py
@@ -2,7 +2,7 @@ import datetime as dt
 import numpy as np
 #import statsmodels.api as sm
 #import statsmodels.formula.api as smf
-
+#from utils import get_san_metric
 import san
 import pandas as pd 
 #all_projects = san.get("projects/all")
@@ -10,41 +10,43 @@ N=3
 
 from_date = '2017-01-01'
 to_date = '2020-12-01'
-asset = 'ethereum'
+#asset = 'bitcoin'
 interval = '1d'
-
+assets=['bitcoin', 'ethereum']#, 'bitcoin']
 # create batch object
-batch = san.Batch()
+#batch = san.Batch()
 
+asset_dict={}
 # create a request
-batch.get(
-    f'daily_active_addresses/{asset}',
-    from_date=from_date,
-    to_date=to_date,
-    interval=interval
-)
-batch.get(
-    f'daily_active_deposits/{asset}',
-    from_date=from_date,
-    to_date=to_date,
-    interval=interval
-)
-batch.get(
-    f'prices/{asset}',
-    from_date=from_date,
-    to_date=to_date,
-    interval=interval
-)
 
-# execute the request
-[daa, dad, price] = batch.execute()
+for asset in assets:
+    all_metrics=san.available_metrics_for_slug(asset)
+    batch = san.Batch()
+    for metric in sorted(all_metrics)[:int(len(all_metrics)/2)]:
+
+    #batch=san.Batch()
+        batch.get(
+            f'{metric}/{asset}',
+            from_date=from_date,
+            to_date=to_date,
+            interval=interval
+        )
+    
+
+    #ex_flow = get_san_metric(from_date, to_date, 'exchange_funds_flow', asset, interval='1d', iterate_over_days=700)
+ 
+    [daa, dad, price, others] = batch.execute()
+    daa['asset']=asset
+    asset_dict[asset] = daa.rename(columns={'value':'activeAddresses', 'asset':'asset'}).join(dad).join(price[['priceUsd', 'priceBtc', 'volume']]).join(others) #.fillna(0)
+    #price['asset'] =asset
+    print(asset, asset_dict[asset].head())
 
 # merge dataframes
-data = daa.rename(columns={'value':'activeAddresses'}).join(dad).join(price['priceUsd'])
-data['asset']=asset
+#data = daa.rename(columns={'value':'activeAddresses'}).join(dad).join(price['priceUsd'])
+#data['asset']=asset
 # take a look
-print(data.head())
-
+#print(data.head())
+#print(data.describe())
 #all_projects.to_csv('all_projects.csv', header=True, encoding='utf-8')
 #print(all_projects.head())
 #pd.DataFrame(all_projects['projectBySlug'], index=[0])
